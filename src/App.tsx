8c8df19c9b948e142aaa478bedfc6649fa2886d4
Hengrui Jia <hari20020511@gmail.com>
2025-10-25 01:52:38 -0400

    added

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React with Rsbuild</h1>
+      <p>Start editing to see some magic happen!</p>
+    </div>
+  );
+}
+
+export default App;
